a77a391b96aa8303ba28eaf39a1aafdcaab1958a
Gunnar Wrobel <p@rdus.de>
2015-01-28 00:19:09 +0100

    s3.5: Die ersten Code-Korrekturen nach dem JSHint Lauf

--- geburtstag.js
+++ geburtstag.js
@@ -1,6 +1,5 @@
 output = function () {
-    return
-        'Gunnar Wrobel hat heute Geburtstag. Kaufe ein Geschenk!';
-}
+    return 'Gunnar Wrobel hat heute Geburtstag. Kaufe ein Geschenk!';
+};
 
 console.log(output())
