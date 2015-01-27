8b2ad552514ade1615386242bf068f4e93b460df
Gunnar Wrobel <p@rdus.de>
2015-01-28 00:46:05 +0100

    s3.7: Ohne Semikola, dafür mit asi: true

--- geburtstag.js
+++ geburtstag.js
@@ -1,7 +1,7 @@
 /* global console */
-/* jshint undef: true */
+/* jshint undef: true, asi: true */
 var output = function () {
-    return 'Gunnar Wrobel hat heute Geburtstag. Kaufe ein Geschenk!';
-};
+    return 'Gunnar Wrobel hat heute Geburtstag. Kaufe ein Geschenk!'
+}
 
-console.log(output());
+console.log(output())
