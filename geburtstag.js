31e866ee0224d147c40facacf92cb8383de220bb
Gunnar Wrobel <p@rdus.de>
2015-01-27 09:02:28 +0100

    s1.5: Ein aufgeblähtes Script für die Debugging Session

--- geburtstag.js
+++ geburtstag.js
@@ -1,1 +1,15 @@
-console.log('Gunnar Wrobel hat heute Geburtstag. Kaufe ein Geschenk!')
+console.log(output())
+
+function geschenk(line) {
+    line = line + ' '
+    line = line + 'Kaufe ein Geschenk!'
+    return line
+}
+
+function output() {
+    var line = 'Gunnar Wrobel hat heute Geburtstag.'
+
+    debugger
+
+    return geschenk(line)
+}
