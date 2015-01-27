a3e46c4172a34552f960e06538c0d4d2a34fc138
Gunnar Wrobel <p@rdus.de>
2015-01-28 00:24:06 +0100

    s3.5: Ausklammern eines Fehlers in einer bestimmten Region einer Datei

--- geburtstag.js
+++ geburtstag.js
@@ -1,6 +1,7 @@
-output = function () {
-    return 'Gunnar Wrobel hat heute Geburtstag. Kaufe ein Geschenk!';
-};
-
 /* jshint -W033 */
+output = function () {
+    return 'Gunnar Wrobel hat heute Geburtstag. Kaufe ein Geschenk!'
+};
+/* jshint +W033 */
+
 console.log(output())
