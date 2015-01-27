8daf2f476526bd6ae08abaf02fc1936c67f0dd2b
Gunnar Wrobel <p@rdus.de>
2015-01-27 22:18:31 +0100

    s2.0: Ein fehlerhafter Zeilenumbruch

--- geburtstag.js
+++ geburtstag.js
@@ -1,5 +1,6 @@
 output = function () {
-    return 'Gunnar Wrobel hat heute Geburtstag. Kaufe ein Geschenk!';
+    return
+        'Gunnar Wrobel hat heute Geburtstag. Kaufe ein Geschenk!';
 }
 
 console.log(output())
