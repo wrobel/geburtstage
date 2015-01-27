174e2542f1dd202f86308508da3e5570b7b840a6
Gunnar Wrobel <p@rdus.de>
2015-01-28 00:26:19 +0100

    s3.6: Alle Semikola gesetzt, aber ein zusätzlicher Tippfehler

--- geburtstag.js
+++ geburtstag.js
@@ -1,7 +1,5 @@
-/* jshint -W033 */
 output = function () {
-    return 'Gunnar Wrobel hat heute Geburtstag. Kaufe ein Geschenk!'
+    return 'Gunnar Wrobel hat heute Geburtstag. Kaufe ein Geschenk!';
 };
-/* jshint +W033 */
 
-console.log(output())
+console.log(outputs());
