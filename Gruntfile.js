254fa142e0a217a8cadcdfc3591fe33a527fdde5
Gunnar Wrobel <p@rdus.de>
2015-01-28 07:42:53 +0100

    s4.4: Eine Variante bei der die Optionen für alle JSHint Aufgaben gelten

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,12 +2,12 @@ module.exports = function (grunt) {
     grunt.initConfig({
         jshint: {
             files: {
-                src: ['Gruntfile.js', 'geburtstag.js'],
-                options: {
-                    jshintrc: true
-                }
+                src: ['Gruntfile.js', 'geburtstag.js']
+            },
+            options: {
+                jshintrc: true
             }
-        },
+        }
     });
 
     grunt.loadNpmTasks('grunt-contrib-jshint');
