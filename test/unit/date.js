394c662a4b4862cb4babe06231841b40bdb9462b
Gunnar Wrobel <p@rdus.de>
2015-01-28 22:24:34 +0100

    s5.2: Ein zweiter Test für die hasBirthday Funktion

--- test/unit/date.js
+++ test/unit/date.js
@@ -13,5 +13,12 @@ describe('date', function () {
             birthday.setFullYear(1974);
             expect(date.hasBirthday(birthday)).to.be.ok;
         });
+
+        it('returns false if the current day and month do not match the given birthday', function () {
+            var birthday = new Date();
+            birthday.setDate(birthday.getDate() + 1);
+            birthday.setFullYear(1974);
+            expect(date.hasBirthday(birthday)).to.be.not.ok;
+        });
     });
 });
