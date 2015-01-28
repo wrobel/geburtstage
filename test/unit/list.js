33c7b3e5621e021d225db1a2d56df331b57f1145
Gunnar Wrobel <p@rdus.de>
2015-01-29 00:20:48 +0100

    s6.5: Der Test des list-Moduls wird mit rewire und Sinon.JS komplettiert

--- test/unit/list.js
+++ test/unit/list.js
@@ -1,6 +1,8 @@
-var expect = require('chai').expect;
+var expect = require('chai').expect,
+    rewire = require('rewire'),
+    sinon = require('sinon');
 
-var list = require('../../lib/list');
+var list = rewire('../../lib/list');
 
 describe('list', function () {
     describe('birthdays', function () {
@@ -9,6 +11,12 @@ describe('list', function () {
                 {name: 'John Doe', birthday: '19741212'},
                 {name: 'Hans Mustermann', birthday: '19700101'}
             ];
+
+            var hasBirthday = sinon.stub();
+            hasBirthday.withArgs('19741212').returns(true);
+            hasBirthday.withArgs('19700101').returns(false);
+            list.__set__('date', { hasBirthday: hasBirthday });
+
             expect(list.birthdays(birthdayList).length).to.equal(1);
         });
     });
