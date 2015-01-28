2a6447e97110439ccf616ee0572f8ffdccceb2ac
Gunnar Wrobel <p@rdus.de>
2015-01-28 08:15:58 +0100

    s4.6: Die Grunt Konfiguarion für die "watch" Aufgabe

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,16 +1,29 @@
 /* global module */
 module.exports = function (grunt) {
     'use strict';
+
     grunt.initConfig({
+        path: {
+            lint: [
+                'Gruntfile.js',
+                'geburtstag.js'
+            ]
+        },
         jshint: {
-            files: ['Gruntfile.js', 'geburtstag.js'],
+            files: '<%= path.lint %>',
             options: {
                 jshintrc: true
             }
+        },
+        watch: {
+            files: '<%= path.lint %>',
+            tasks: ['jshint']
         }
     });
 
     grunt.loadNpmTasks('grunt-contrib-jshint');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-notify');
 
     grunt.registerTask('default', 'jshint');
 };
