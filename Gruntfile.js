dbe1cc181fe0db7733c22f099742091d4eb8affb
Gunnar Wrobel <p@rdus.de>
2015-01-28 07:36:12 +0100

    s4.4: Grunt kann nun die JSHint-Prüfung anstossen

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,4 +1,16 @@
 module.exports = function (grunt) {
     grunt.initConfig({
+        jshint: {
+            files: {
+                src: ['Gruntfile.js', 'geburtstag.js'],
+                options: {
+                    jshintrc: true
+                }
+            }
+        },
     });
+
+    grunt.loadNpmTasks('grunt-contrib-jshint');
+
+    grunt.registerTask('default', 'jshint');
 };
